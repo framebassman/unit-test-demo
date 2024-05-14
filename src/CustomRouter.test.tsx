99bdec4880086a30277a692e2c92cf788da8167d
Dmitry Romashov <framebassman@gmail.com>
2024-05-14 12:15:58 +0200

    Remove warning and add debug configuration

--- src/CustomRouter.test.tsx
+++ src/CustomRouter.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { CustomRouter } from './CustomRouter';
@@ -8,10 +8,8 @@ test('from App to CustomForm', async () => {
   render(<CustomRouter/>);
   const user = userEvent.setup();
 
-  // verify page content for default route
   expect(screen.getByText(/Navigate To Form/i)).toBeInTheDocument();
 
-  // verify page content for expected route after navigating
   await user.click(screen.getByText(/Navigate To Form/i));
   expect(screen.getByText(/Subscribe!/i)).toBeInTheDocument();
 })
