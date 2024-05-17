0a2c6f72691bfab4828c5105e355660d53604630
Dmitry Romashov <framebassman@gmail.com>
2024-05-17 23:27:18 +0200

    Add basic example

--- src/CustomRouter.tsx
+++ src/CustomRouter.tsx
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import App from './App';
 import { CustomForm } from './CustomForm';
+import { Anonimus } from './Anonimus';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/form",
     element: <CustomForm/>,
+  },
+  {
+    path: "anonimus",
+    element: <Anonimus/>,
   },
 ]);
 
